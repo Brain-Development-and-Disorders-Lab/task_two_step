7fe7d0e5faff2450bc17c17ae6f84050e70d4f31
Henry Burgess <henryjburg@gmail.com>
2022-01-10 14:31:43 -0600

    Clean-up
    Updated `jspsych-d3-instructions.js`

--- src/lib/plugins/jspsych-d3-instructions.js
+++ src/lib/plugins/jspsych-d3-instructions.js
@@ -5,7 +5,6 @@
  * VF 8/2019
  * KN added audio 4/22/20
  **/
-
 import {select} from 'd3';
 import {
   width,
@@ -30,27 +29,27 @@ jsPsych.plugins['d3-instructions'] = (function() {
         type: jsPsych.plugins.parameterType.STRING,
         pretty_name: 'Stimulus',
         default: null,
-        description: 'The image to be displayed'
+        description: 'The image to be displayed',
       },
       right_text: {
         type: jsPsych.plugins.parameterType.STRING,
         default: null,
-        array: false
+        array: false,
       },
       left_text: {
         type: jsPsych.plugins.parameterType.STRING,
         default: null,
-        array: false
+        array: false,
       },
       center_text: {
         type: jsPsych.plugins.parameterType.STRING,
         default: null,
-        array: false
+        array: false,
       },
       reward_string: {
         type: jsPsych.plugins.parameterType.STRING,
         default: null,
-        array: false
+        array: false.valueOf,
       },
       choices: {
         type: jsPsych.plugins.parameterType.KEYCODE,
@@ -64,13 +63,13 @@ jsPsych.plugins['d3-instructions'] = (function() {
         type: jsPsych.plugins.parameterType.STRING,
         pretty_name: 'Prompt',
         default: null,
-        description: 'Any content here will be displayed below the stimulus.'
+        description: 'Any content here will be displayed below the stimulus.',
       },
       response_ends_trial: {
         type: jsPsych.plugins.parameterType.BOOL,
         pretty_name: 'Response ends trial',
         default: false,
-        description: 'If true, trial will end when subject makes a response.'
+        description: 'If true, trial will end when subject makes a response.',
       },
       button_clicked: {
         type: jsPsych.plugins.parameterType.BOOL,
@@ -78,99 +77,101 @@ jsPsych.plugins['d3-instructions'] = (function() {
         default: false,
         description: 'state of button.',
       },
-    }
-  }
+    },
+  };
 
   plugin.trial = function(display_element, trial) {
     // display stimulus
-    let new_html='<div id="container" class="exp-container"></div>';
+    const new_html = `<div id='container' class='exp-container'></div>`;
     display_element.innerHTML = new_html;
 
-    var move_possible = true;
+    const svg = select('div#container')
+        .append('svg')
+        .attr('preserveAspectRatio', 'xMinYMin meet')
+        .attr('viewBox', '0 0 ' + width + ' ' + height)
+        .classed('svg-content', true);
 
-    var svg = select("div#container")
-      .append("svg")
-      .attr("preserveAspectRatio", "xMinYMin meet")
-      .attr("viewBox", "0 0 " + width + " " + height)
-      .classed("svg-content", true);
+    if (trial.stimulus !== null) {
+      svg.append('svg:image')
+          .attr('width', width)
+          .attr('height', height)
+          .attr('xlink:href', trial.stimulus);
+    }
 
-
-    if (trial.stimulus !== null){
-    var main_image = svg.append("svg:image")
-        .attr('width', width)
-        .attr('height', height)
-        .attr("xlink:href", trial.stimulus)}
-
-    if (trial.right_text !== null){
-      var right_image = svg.append("svg:image")
-        .attr('class', 'right')
-        .attr('x', choice_x_right)
-        .attr('y', choice_y)
-        .attr('width', monster_size)
-        .attr('height', monster_size)
-        .attr("xlink:href", trial.right_text)}
-
-    if (trial.reward_string !== null){
-      var reward_image = svg.append("svg:image")
-        .attr('x', x_center-reward_size/2)
-        .attr('y', choice_y)
-        .attr('width', reward_size)
-        .attr('height', reward_size)
-        .attr("xlink:href", trial.reward_string);
-      }
-
-    if (trial.left_text !== null){
-    var left_image = svg.append("svg:image")
-        .attr('x', choice_x_left)
-        .attr('y', choice_y)
-        .attr('width', monster_size)
-        .attr('height', monster_size)
-        .attr("xlink:href", trial.left_text)}
-
-    if (trial.center_text !== null) {
-      var center_image = svg.append("svg:image")
-          .attr('x', x_center-monster_size/2)
+    if (trial.right_text !== null) {
+      svg.append('svg:image')
+          .attr('class', 'right')
+          .attr('x', choice_x_right)
           .attr('y', choice_y)
           .attr('width', monster_size)
           .attr('height', monster_size)
-          .attr("xlink:href", trial.center_text) }
+          .attr('xlink:href', trial.right_text);
+    }
 
-    var button_image = svg.append("svg:circle")
+    if (trial.reward_string !== null) {
+      svg.append('svg:image')
+          .attr('x', x_center-reward_size / 2)
+          .attr('y', choice_y)
+          .attr('width', reward_size)
+          .attr('height', reward_size)
+          .attr('xlink:href', trial.reward_string);
+    }
+
+    if (trial.left_text !== null) {
+      svg.append('svg:image')
+          .attr('x', choice_x_left)
+          .attr('y', choice_y)
+          .attr('width', monster_size)
+          .attr('height', monster_size)
+          .attr('xlink:href', trial.left_text);
+    }
+
+    if (trial.center_text !== null) {
+      svg.append('svg:image')
+          .attr('x', x_center-monster_size / 2)
+          .attr('y', choice_y)
+          .attr('width', monster_size)
+          .attr('height', monster_size)
+          .attr('xlink:href', trial.center_text);
+    }
+
+    const button_image = svg.append('svg:circle')
         .attr('r', 25)
         .attr('cx', choice_x_right + monster_size)
         .attr('cy', choice_y + monster_size - 100)
-        .style("stroke", "black")
-        .style("fill", "red")
-        .on("click", function() {trial.button_clicked = true; console.log(trial.button_clicked);
-          select(this).style("fill", "green");
-          select(this).style("stroke", "black");
-        })
+        .style('stroke', 'black')
+        .style('fill', 'red')
+        .on('click', () => {
+          trial.button_clicked = true;
+          console.log(trial.button_clicked);
+          button_image.style('fill', 'green');
+          button_image.style('stroke', 'black');
+        });
 
-  
     // add prompt
-    if (trial.prompt !== null){
-      //inspired by Mike Bostock
+    if (trial.prompt !== null) {
+      // inspired by Mike Bostock
       // https://bl.ocks.org/mbostock/7555321
-      var lineNumber = 0,
-      lineHeight = 1.1, // ems
-      dy = 0; 
-      for (var i=0; i<trial.prompt.length; i++){
-        svg.append("text").attr("x", text_start_x).attr("y", instructions_text_start_y).attr("dy", ++lineNumber * lineHeight + dy + "em").style("font-size", font_size+"px").style('fill', 'white').text(trial.prompt[i]);
-        lineNumber ++ ;}
+      let lineNumber = 0;
+      const lineHeight = 1.1; // ems
+      const dy = 0;
+      for (let i = 0; i < trial.prompt.length; i++) {
+        svg.append('text')
+            .attr('x', text_start_x)
+            .attr('y', instructions_text_start_y)
+            .attr('dy', ++lineNumber * lineHeight + dy + 'em')
+            .style('font-size', font_size+'px')
+            .style('fill', 'white')
+            .text(trial.prompt[i]);
+        lineNumber++;
+      }
     }
 
-    /*/ store response
-    var response = {
-      rt: null,
-      key: null
-    }; */
-
     // function to end trial when it is time
-    var end_trial = function() {
+    const end_trial = () => {
+      console.log('end trial initiated');
 
-      console.log('end trial initiated')
-
-      //set button_clicked back to false
+      // set button_clicked back to false
       trial.button_clicked = false;
 
       // kill any remaining setTimeout handlers
@@ -181,11 +182,6 @@ jsPsych.plugins['d3-instructions'] = (function() {
         jsPsych.pluginAPI.cancelKeyboardResponse(keyboardListener);
       }
 
-      // gather the data to store for the trial
-      //var trial_data = {
-       // "rt": response.rt
-      //}; 
-
       // clear the display
       display_element.innerHTML = '';
 
@@ -194,29 +190,24 @@ jsPsych.plugins['d3-instructions'] = (function() {
     };
 
 
-// function to handle responses by the subject
-var after_response = function() {
-
-  // only record the first response
-  //if (response.key == null) {
-  //  response = info;
-  //}
-      if(trial.button_clicked == true){
-          end_trial();
-        };
+    // function to handle responses by the subject
+    const after_response = () => {
+      if (trial.button_clicked == true) {
+        end_trial();
       };
+    };
 
-// start the response listener
-if (trial.choices != jsPsych.NO_KEYS) {
-  var keyboardListener = jsPsych.pluginAPI.getKeyboardResponse({
-    callback_function: after_response,
-    valid_responses: trial.choices,
-    rt_method: 'performance',
-    persist: true,
-    allow_held_key: false,
-  });
-} 
- };
+    // start the response listener
+    if (trial.choices != jsPsych.NO_KEYS) {
+      jsPsych.pluginAPI.getKeyboardResponse({
+        callback_function: after_response,
+        valid_responses: trial.choices,
+        rt_method: 'performance',
+        persist: true,
+        allow_held_key: false,
+      });
+    }
+  };
 
   return plugin;
 })();
